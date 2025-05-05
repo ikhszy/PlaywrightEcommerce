0fb92d93f4e287a19c2cc0fd4d082295d4726093
ikhsan <ikhszy@gmail.com>
2025-05-05 10:29:33 +0700

    add json testdata and sample cases on how to use it

--- page-objects/baseHelper.ts
+++ page-objects/baseHelper.ts
@@ -1,6 +1,8 @@
 // @ts-nocheck
 import { expect, Locator, Page } from '@playwright/test'
 import { fakerEN, fakerEN_US } from '@faker-js/faker'
+import test from 'node:test'
+const testData = JSON.parse(JSON.stringify(require('../test-data/registerUser.json')))
 
 export class baseHelper{
     readonly page: Page
@@ -64,5 +66,20 @@ export class baseHelper{
         this.state = fakerEN_US.location.state()
         this.loginname = 'login_' + this.lastname
         this.password = 'pass1234'
+    }
+
+    async getRegisterDataJSON() {
+        this.firstname = testData.firstname
+        this.lastname = testData.lastname
+        this.email = testData.email
+        this.phone = testData.phone
+        this.company = testData.company
+        this.address1 = testData.address1
+        this.city = testData.city
+        this.zipcode = testData.zipcode
+        this.country = testData.country
+        this.state = testData.state
+        this.loginname = testData.loginname
+        this.password = testData.password
     }
 }
